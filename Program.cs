c910508ca16eb107d9a027732ddd035ee37618dc
IvanShmytov <ivanshmytov@mail.ru>
2022-04-30 15:52:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string DirName = @"C:\Users";
+        Console.WriteLine($"Размер папки {DirName} - {ShowFolderSize(DirName)} байт.");
+    }
+    static long ShowFolderSize(string dirName)
+    {
+        long folderSize = 0;
+        try
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+            if (dirInfo.Exists)
+            {
+                FileInfo[] files = dirInfo.GetFiles();
+                foreach (var item in files)
+                {
+                    folderSize += item.Length;
+                }
+                DirectoryInfo[] subfolders = dirInfo.GetDirectories();
+                foreach (var item in subfolders)
+                {
+                    folderSize += ShowFolderSize(item.FullName);
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Указан неверный путь к директории");
+            }           
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        return folderSize;
+    }
+}
